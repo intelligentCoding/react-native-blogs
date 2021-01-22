f3f30eddc6bb4ecea1cf1bda181d6dae884049d3
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-22 12:25:20 -0500

    Blog post show screen created

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
 import {Context} from "../context/BlogContext";
 import { Feather } from '@expo/vector-icons';
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
@@ -11,12 +11,18 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPosts) => blogPosts.id}
         renderItem={({ item }) => {
-          return <View style={styles.row}>
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate("ShowScreen", {id: item.id})}>
+
+          <View style={styles.row}>
               <Text style={styles.title}>{item.title} - {item.id}</Text>
               <TouchableOpacity onPress={()=>{ deleteBlogPost(item.id)}}>
               <Feather name="trash" style={styles.icon}/>
               </TouchableOpacity>
               </View>
+            </TouchableOpacity>
+          )
+          
         }}
       />
     </View>
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Text, View, StyleSheet} from 'react-native';
+import {Context } from '../context/BlogContext'
 
-
-const ShowScreen = () => {
+const ShowScreen = ({navigation}) => {
+    const {state} = useContext(Context);
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
     return (
          <View>
-            <Text>Show screen</Text>
+            <Text>{blogPost.title}</Text>
         </View>
     )
 }
