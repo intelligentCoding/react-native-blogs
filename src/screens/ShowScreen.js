eadbc9fc50aff694938ca0f09a555bca6cbafc02
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-01-22 11:04:05 -0500

    Delete blog is setup with context

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native';
+
+
+const ShowScreen = () => {
+    return (
+         <View>
+            <Text>Show screen</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
+
+export default ShowScreen;
